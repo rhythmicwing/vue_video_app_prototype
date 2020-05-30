b5a55af746d235e9029c58920cbd4368ab6d884a
Esla Mah <elle.erusa.us@gmail.com>
2020-05-31 02:46:23 +0800

    adding files

---
+++ src/components/searchIso.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="search-iso">
+    <div>search here</div>
+    <input type="text" v-model="request" @keyup="resultBack(request)" />
+
+    <div v-for='result in results' :key='result.id'>
+      <p>{{result.title}}</p>
+      <img :src="searchThumbnails + result.poster_path">
+    </div>
+
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'search-iso',
+  
+  data() {
+    return {
+      request: '',
+      results: '',
+      searchThumbnails: 'http://image.tmdb.org/t/p/w185/',
+    }
+  },
+  methods: {
+    resultBack(request){
+      if(this.request != 0) {
+        axios.get("https://api.themoviedb.org/3/search/movie?api_key=0a2c307e58764f38de216f1c5e2d7384&query=" + request).then(response => { this.results = response.data.results })
+        console.log(this.results)
+      }
+      
+    }
+  }
+
+}
+</script>
+
+
+<style lang="scss" scoped>
+
+
+
+</style>
---
+++ src/components/testpage.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="search-iso">
+    testing here
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'search-iso',
+  
+  data() {
+    return {
+      request: '',
+      results: '',
+      searchThumbnails: 'http://image.tmdb.org/t/p/w185/',
+    }
+  },
+  
+
+}
+</script>
+
+
+<style lang="scss" scoped>
+
+
+
+</style>
